0451d309a17af0731d4ca0e84838388f1180b2e1
stkzk3110 <fisherman0825dd@gmail.com>
2019-06-27 17:06:47 +0900

    twelveth commit

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -14,7 +14,7 @@ class PhotoController extends Controller
     public function __construct()
     {
         // 認証が必要
-        $this->middleware('auth')->except(['index', 'download']);
+        $this->middleware('auth')->except(['index', 'show']);
     }
 
     /**
@@ -25,6 +25,18 @@ class PhotoController extends Controller
         $photos = Photo::with(['owner'])
             ->orderBy(Photo::CREATED_AT, 'desc')->paginate();
         return $photos;
+    }
+
+    /**
+     * 写真詳細
+     * @param string $id
+     * @return Photo
+     */
+    public function show(string $id)
+    {
+        $photo = Photo::where('id', $id)->with(['owner'])->first();
+
+        return $photo ?? abort(404);
     }
 
     /**
