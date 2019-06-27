9200be1042986bf19a3e2a689c1759fc3c4cc63b
stkzk3110 <fisherman0825dd@gmail.com>
2019-06-27 17:59:44 +0900

    fourtennth commit

--- app/Photo.php
+++ app/Photo.php
@@ -1,39 +1,30 @@
 <?php
-
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
-
+use Illuminate\Support\Facades\Auth;
 class Photo extends Model
 {
     /** プライマリキーの型 */
     protected $keyType = 'string';
-
     /** IDの桁数 */
     const ID_LENGTH = 12;
-
-    protected $perPage = 15;
-
-    /** JSONに含める属性 */
+    /** JSONに含めるアクセサ */
     protected $appends = [
-        'url',
+        'url', 'likes_count', 'liked_by_user',
     ];
-
     /** JSONに含める属性 */
     protected $visible = [
-        'id', 'owner', 'url', 'comments'
+        'id', 'owner', 'url', 'comments',
+        'likes_count', 'liked_by_user',
     ];
-
     public function __construct(array $attributes = [])
     {
         parent::__construct($attributes);
-
         if (! array_get($this->attributes, 'id')) {
             $this->setId();
         }
     }
-
     /**
      * ランダムなID値をid属性に代入する
      */
@@ -41,7 +32,6 @@ class Photo extends Model
     {
         $this->attributes['id'] = $this->getRandomId();
     }
-
     /**
      * ランダムなID値を生成する
      * @return string
@@ -52,18 +42,13 @@ class Photo extends Model
             range(0, 9), range('a', 'z'),
             range('A', 'Z'), ['-', '_']
         );
-
         $length = count($characters);
-
         $id = "";
-
         for ($i = 0; $i < self::ID_LENGTH; $i++) {
             $id .= $characters[random_int(0, $length - 1)];
         }
-
         return $id;
     }
-
     /**
      * アクセサ - url
      * @return string
@@ -72,7 +57,27 @@ class Photo extends Model
     {
         return Storage::cloud()->url($this->attributes['filename']);
     }
-
+    /**
+     * アクセサ - likes_count
+     * @return int
+     */
+    public function getLikesCountAttribute()
+    {
+        return $this->likes->count();
+    }
+    /**
+     * アクセサ - liked_by_user
+     * @return boolean
+     */
+    public function getLikedByUserAttribute()
+    {
+        if (Auth::guest()) {
+            return false;
+        }
+        return $this->likes->contains(function ($user) {
+            return $user->id === Auth::user()->id;
+        });
+    }
     /**
      * リレーションシップ - usersテーブル
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
@@ -81,7 +86,6 @@ class Photo extends Model
     {
         return $this->belongsTo('App\User', 'user_id', 'id', 'users');
     }
-
     /**
      * リレーションシップ - commentsテーブル
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
@@ -89,5 +93,13 @@ class Photo extends Model
     public function comments()
     {
         return $this->hasMany('App\Comment')->orderBy('id', 'desc');
+    }
+    /**
+     * リレーションシップ - usersテーブル
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function likes()
+    {
+        return $this->belongsToMany('App\User', 'likes')->withTimestamps();
     }
 }
