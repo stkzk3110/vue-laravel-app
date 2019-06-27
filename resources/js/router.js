8b78e206373f2ad3f5ac9691a68b0f549d1445c9
stkzk3110 <fisherman0825dd@gmail.com>
2019-06-27 14:00:20 +0900

    nineth commit

--- resources/js/router.js
+++ resources/js/router.js
@@ -9,6 +9,8 @@ import store from './store'
 
 import SystemError from './pages/errors/System.vue'
 
+import PhotoDetail from './pages/PhotoDetail.vue'
+
 // VueRouterプラグインを使用する
 // これによって<RouterView />コンポーネントなどを使うことができる
 Vue.use(VueRouter)
@@ -18,6 +20,11 @@ const routes = [
   {
     path: '/',
     component: PhotoList
+  },
+  {
+    path: '/photos/:id',
+    component: PhotoDetail,
+    props: true
   },
   {
     path: '/login',
